f92648a48ddb8ccb9e0dbc3bfc45978283770c32
Annette Pan <apan38@gatech.edu>
2022-11-23 12:01:06 -0500

    Get rid of console errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from './App';
 
-// ReactDOM.render(
-//     <h1>Hello, Create React App!</h1>,
-//     document.getElementById("root")
-// )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
