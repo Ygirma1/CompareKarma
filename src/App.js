024a598b3853f09d9d7898d2f30ca6e55d50be6a
Annette Pan <apan38@gatech.edu>
2022-11-23 14:57:45 -0500

    Added filter by course type

--- src/App.js
+++ src/App.js
@@ -11,14 +11,28 @@ const base_url = "http://localhost:8080"
 function App() {
     const [query, setQuery] = useState("");
     const [data, setData] = useState([]);
+    const [filterParam, setFilterParam] = useState(["All"]);
 
     // search by company name, course name, and course type
     const keys = ["company_name", "course_name", "course_type"];
+    
+    // search and filter while typing
     const search = (data) => {
-        return data.filter(
-            (item) =>
-              keys.some((key)=>item[key].toLowerCase().includes(query.toLowerCase()))
-        );
+        return data.filter((item) => {
+            if (item.course_type == filterParam) {
+                return keys.some((key) => {
+                    return (
+                        item[key].toLowerCase().includes(query.toLowerCase())
+                    )
+                })
+            } else if (filterParam == "All") {
+                return keys.some((key) => {
+                    return (
+                        item[key].toLowerCase().includes(query.toLowerCase())
+                    )
+                })
+            }
+        })
     }
 
     // getting data from backend
@@ -34,15 +48,25 @@ function App() {
     return (
         <div className='app'>
             <form className='search'>
-            <input 
-                placeholder='Search...' 
-                className='search-input' 
-                onChange={e=> setQuery(e.target.value)}>
-            </input>
-            <button className='search-button'>
-                <SearchIcon/>
-            </button>
+                <input 
+                    placeholder='Search...' 
+                    className='search-input' 
+                    onChange={e=> setQuery(e.target.value)}>
+                </input>
+                <button className='search-button'>
+                    <SearchIcon/>
+                </button>
             </form>
+            <select
+                onChange={(e) => {
+                    setFilterParam(e.target.value);
+                }}
+                className="custom-select"
+                aria-label='Filter by Course Type'>
+                    <option value="All">Filter by Course Type</option>
+                    <option value="UI/UX">UI/UX</option>
+                    <option value="SWE">SWE</option>
+            </select>
             {<Table data={search(data)}/>}
         </div>
     );
