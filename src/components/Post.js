dc47c2d1ce499946c6738749b69a733bb6f07513
Annette Pan <apan38@gatech.edu>
2022-11-23 12:32:29 -0500

    Added some styling

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+const Post = ({ post }) => {
+    return (
+        <article>
+            <h2 className="company-name">{post.company_name}</h2>
+            <p>{post.description_of_bootcamp}</p>
+            <p>Course Type: {post.course_type}</p>
+        </article>
+    )
+}
+
+export default Post;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
+import Post from './Post';
 
 const Table = ({data}) => {
+    const results = data.map(post => <Post key={post.company_name} post={post} />)
     return (
-        <table>
-            <tbody>
-                <tr>
-                    <th>Company Name</th>
-                    <th>Course Name</th>
-                    <th>Description</th>
-                    <th>Course Type</th>
-                    <th>Course Length</th>
-                </tr>
-                {data.map((item) => (
-                <tr key={item.company_name}>
-                    <td>{item.company_name}</td>
-                    <td>{item.course_name}</td>
-                    <td>{item.description_of_bootcamp}</td>
-                    <td>{item.course_type}</td>
-                    <td>{item.length_of_course}</td>
-                </tr>
-                ))}
-            </tbody>
-        </table>
+        <main>{results}</main>
+        // <table>
+        //     <tbody>
+        //         <tr>
+        //             <th>Company Name</th>
+        //             <th>Course Name</th>
+        //             <th>Description</th>
+        //             <th>Course Type</th>
+        //             <th>Course Length</th>
+        //         </tr>
+        //         {data.map((item) => (
+        //         <tr key={item.company_name}>
+        //             <td>{item.company_name}</td>
+        //             <td>{item.course_name}</td>
+        //             <td>{item.description_of_bootcamp}</td>
+        //             <td>{item.course_type}</td>
+        //             <td>{item.length_of_course}</td>
+        //         </tr>
+        //         ))}
+        //     </tbody>
+        // </table>
     )
 };
 
