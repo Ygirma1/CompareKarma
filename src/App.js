8ed578c879f701d6cb3785427652397df5396c8b
Annette Pan <apan38@gatech.edu>
2022-11-23 11:04:16 -0500

    Basic search with hardcoded json

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import {Bootcamps} from "./bootcamps";
 
 const base_url = "http://localhost:8080"
 
+
 function App() {
-    const [obj, setObj] = useState({});
-    const [sort, setSort] = useState({});
-    const [filterType, setFilterType] = useState([]);
-    const [page, setPage] = useState(1);
-    const [search, setSearch] = useState("");
+    // const [data, setData] = useState([]);
 
-    useEffect(() => {
-        const getAllBootCamps = async() => {
-            try {
-                const url = `${base_url}/query`;
-                const { data } = await axios.get(url);
-                setObj(data);
-                console.log(data);
+    // useEffect(() => {
+    //     var getBootCamps = async() => {
+    //         try {
+    //             const url = `${base_url}/query`;
+    //                 const { data } = await axios.get(url);
+    //                 console.log(data);
+                    
+    //             } catch(err) {
+    //                 console.log(err);
+    //             }
+    //         }
+    // })
+    const [query, setQuery] = useState("");
+    return (
+        <div className='app'>
+            <input 
+                type='text' 
+                placeholder='Search...' 
+                className='search' 
+                onChange={e=> setQuery(e.target.value)}>
+            </input>
+            <ul className='list'>
+                {Bootcamps.filter(bootcamp=>bootcamp.company_name.toLowerCase().includes(query)).map((bootcamp) => (
+                    <li 
+                        key={bootcamp.company_name}
+                        className='listItem'>{bootcamp.company_name}
+                    </li>
+                ))}
                 
-            } catch(err) {
-                console.log(err);
-            }
-        };
-
-        getAllBootCamps();
-    }, [sort, filterType, page, search]);
-
-    return <h1>Hello World</h1>;
-    
+            </ul>
+        </div>
+    )
 }
 
 export default App;
