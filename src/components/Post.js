15980adbb235a19234a400880fb9d08d255ee036
Annette Pan <apan38@gatech.edu>
2022-11-25 17:19:24 -0500

    Added styling for container

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,22 +5,22 @@ const Post = ({ post }) => {
         <article>
             <h2 className="company-name">{post.company_name}</h2>
             <p className="container">
-                <p className="margin">
+                <p className="margin1">
                     <text className="underline">Course Type:</text>
                     <br></br>
                     <text> {post.course_type}</text>
                 </p>
-                <p className="margin">
+                <p className="margin2">
                     <text className="underline">Class Format:</text>
                     <br></br>
                     <text> {post.course_format}</text>
                 </p>
-                <p className="margin">
+                <p className="margin3">
                     <text className="underline">Length:</text>
                     <br></br>
                     <text> {post.length_of_course}</text>
                 </p>
-                <p className="margin">
+                <p className="margin4">
                     <text className="underline">Cost of Attendance:</text>
                     <br></br>
                     <text> {post.cost}</text>
