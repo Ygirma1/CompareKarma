4f07771d82a94ad8181df676e620cdea8c50a4be
Annette Pan <apan38@gatech.edu>
2023-02-11 19:29:12 -0500

    Basic non functional login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 const Login= () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Login</h1>
-    </div>
+    <>
+        <form onSubmit={handleSubmit}>
+            <label for="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'></input>
+            <label for="password">password</label>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'></input>
+            <button type='submit'>Log In</button>
+        </form>
+        <button>Don't have an account? Register here.</button>
+    </>
   );
 };
 
