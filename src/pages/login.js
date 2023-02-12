5e3f5e56b8890668ea041857ec3e1642d5f8393a
Annette Pan <apan38@gatech.edu>
2023-02-11 21:06:35 -0500

    working login and register logic

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 import React, { useState }from 'react';
 
-const Login= () => {
+const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
@@ -12,13 +12,13 @@ const Login= () => {
   return (
     <>
         <form onSubmit={handleSubmit}>
-            <label for="email">email</label>
+            <label htmlFor="email">email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'></input>
-            <label for="password">password</label>
+            <label htmlFor="password">password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'></input>
             <button type='submit'>Log In</button>
         </form>
-        <button>Don't have an account? Register here.</button>
+        <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
     </>
   );
 };
