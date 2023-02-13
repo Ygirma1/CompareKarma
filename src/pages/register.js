4fa557f25663ac95b722b95a9610ab5e889466e4
Annette Pan <apan38@gatech.edu>
2023-02-13 08:56:13 -0500

    Add business registration to database

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,25 +1,70 @@
-import React, { useState }from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const base_url = "http://localhost:8080"
 
 const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
     const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [desc, setDesc] = useState('');
 
+    // handles submission of form
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
-    }
+        const rest = axios.put(`${base_url}/newUser?`+
+            'business_name=' + name + 
+            '&phone_number=' + phone +
+            '&business_desc=' + desc + 
+            '&verified=0' + 
+            '&profit_status=1' + 
+            '&email=' + email +
+            '&course_type=Online' +
+            '&business_password=' + pass)
+    };
 
   return (
     <div className='auth-form-container'>
         <form className='register-form' onSubmit={handleSubmit}>
             <label htmlFor="name">Business Name</label>
-            <input value={name} onChange={(e) => setName(e.target.value)} name="name" id="name" placeholder="Business Name" ></input>
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'></input>
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'></input>
-            <button type='submit'>Log In</button>
+            <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Business Name">
+            </input>
+            
+            <label htmlFor="email">Email</label>
+            <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder='youremail@gmail.com'>
+            </input>
+            
+            <label htmlFor="name">Phone Number</label>
+            <input
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                placeholder="xxx-xxx-xxxx">
+            </input>
+            
+            <label htmlFor="name">Description</label>
+            <input
+                value={desc}
+                onChange={(e) => setDesc(e.target.value)}
+                placeholder="Description">
+            </input>
+
+            <label htmlFor="password">Password</label>
+            <input
+                value={pass}
+                onChange={(e) => setPass(e.target.value)}
+                type="password"
+                placeholder='********'>
+            </input>
+            
+            <button type='submit'>Sign Up</button>
         </form>
         <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
     </div>
