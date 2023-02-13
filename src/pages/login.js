b06660c811699c0c52dede4a69a955ff5da45cba
Annette Pan <apan38@gatech.edu>
2023-02-13 16:57:01 -0500

    Draft of login logic, works when credentials are correct

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,19 @@
 import React, { useState }from 'react';
+import axios from 'axios';
+
+const base_url = "http://localhost:8080"
 
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault();
-        console.log(email);
+        console.log(email, pass);
+        const res = await axios.get(`${base_url}/verifyUser?` + 
+          'email=' + email +
+          '&password=' + pass);
+        console.log(res.data.status);
     }
 
   return (
@@ -16,7 +23,7 @@ const Login = (props) => {
             <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'></input>
             <label htmlFor="password">password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'></input>
-            <button type='submit'>Log In</button>
+            <button  type='submit'>Log In</button>
         </form>
         <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
     </div>
