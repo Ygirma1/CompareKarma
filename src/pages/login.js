62a93c745e1e085557324c0e43b806b117bda0bc
Annette Pan <apan38@gatech.edu>
2023-02-13 20:33:25 -0500

    Error message for incorrect login

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,6 +6,7 @@ const base_url = "http://localhost:8080"
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = async(e) => {
         e.preventDefault();
@@ -14,16 +15,42 @@ const Login = (props) => {
           'email=' + email +
           '&password=' + pass);
         console.log(res.data.status);
+        if (res.data.status) {
+          console.log("TAKE TO HOME PAGE")
+          window.location.href='/';
+        } else {
+          console.log("ERROR MESSAGE")
+          setErrorMessage('Incorrect Login!');
+         
+        }
+
     }
 
   return (
     <div className='auth-form-container'>
         <form className='login-form' onSubmit={handleSubmit}>
-            <label htmlFor="email">email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'></input>
-            <label htmlFor="password">password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'></input>
-            <button  type='submit'>Log In</button>
+            <label htmlFor="email">Email</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder='youremail@gmail.com'>
+            </input>
+
+            <label htmlFor="password">Password</label>
+            <input
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+              type="password"
+              placeholder='********'>
+            </input>
+
+            <button type='submit'>Log In</button>
+
+            {errorMessage && (
+            <p className="error"> {errorMessage} </p>
+          )}
+
         </form>
         <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
     </div>
