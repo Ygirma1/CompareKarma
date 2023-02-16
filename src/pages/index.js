f1b0998ae0482b2e15f9f914e7b2612b6c4aa9f5
ojoudeh3 <ojoudeh3@gatech.edu>
2023-02-15 19:23:00 -0500

    dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,39 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh'
-      }}
-    >
-      <h1>Home</h1>
+  <body>
+
+<div className="header">
+  <h1>CompareKarma</h1>
+</div>
+
+
+<div className="row">
+  <div className="leftcolumn">
+    <div className="card">
+      <h2 >Bootcamp 1</h2>
+      <h5 >Post date: February 12, 2023</h5>
+      <p>*****(###)</p>
+      <p >Ipsum morbi nascetur a placerat pretium adipiscing Auctor per cum. Sem, nam aliquet metus dapibus. Cum semper suspendisse diam natoque ipsum at mauris, magnis platea bibendum venenatis massa. Aliquet convallis laoreet imperdiet vitae dui ultricies vehicula, porttitor ac pede consequat.</p>
     </div>
+    <div className="card">
+      <h2 >Bootcamp 2</h2>
+      <h5 >Post date: February 14, 2023</h5>
+      <p>*****(###)</p>
+      <p >Ipsum morbi nascetur a placerat pretium adipiscing Auctor per cum. Sem, nam aliquet metus dapibus. Cum semper suspendisse diam natoque ipsum at mauris, magnis platea bibendum venenatis massa. Aliquet convallis laoreet imperdiet vitae dui ultricies vehicula, porttitor ac pede consequat.</p>
+    </div>
+  </div>
+  <div className="rightcolumn">
+    <div className="card">
+      <h2 >Total Review</h2>
+      <p >Average stars out of five for the entire account.</p>
+      <p >Some text about total reviews of all the account's bootcamps</p>
+    </div>
+  </div>
+</div>
+
+
+</body>
   );
 };
 
