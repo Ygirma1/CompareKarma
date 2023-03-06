654583fd0b07a2da8edcd3f34cc4e2f09365c91f
Annette Pan <apan38@gatech.edu>
2023-03-06 17:23:47 -0500

    New bootcamp post gets added to db

--- src/pages/businessPost/businessPost.js
+++ src/pages/businessPost/businessPost.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './businessPost.css';
+import axios from 'axios';
 import Multiselect from 'multiselect-react-dropdown';
 import { InputLabel, Select, FormControl, MenuItem, Box } from '@material-ui/core';
+
+const base_url = "http://localhost:8080"
 
 const BusinessPost = (props) => {
   const [price, setPrice] = useState('');
@@ -10,10 +13,22 @@ const BusinessPost = (props) => {
   const [link, setLink] = useState('');
   const [format, setFormat] = useState('');
   const [courseTypes, setCourseTypes] = useState([]);
+  const [length, setLength] = useState('');
 
   const handleSubmit = async(e) => {
     e.preventDefault();
     console.log(price, desc, name, link, courseTypes, format);
+
+    const res = axios.put(`${base_url}/newBusinessCourse?` + 
+    'company_name=' + 'TEST' +
+    '&course_format=' + format + 
+    '&course_name=' + name + 
+    '&length_of_course=' + length + 
+    '&cost=' + price + 
+    '&description_of_bootcamp=' + desc + 
+    '&course_type=' + courseTypes + 
+    '&business_id=' + 1)
+
   };
 
   const handleSelect = (selectedList) => {
@@ -31,7 +46,7 @@ const BusinessPost = (props) => {
 
   return (
     <div className='post-form-container'>
-    <form className='post-form' onClick={handleSubmit}>
+    <form className='post-form' onSubmit={handleSubmit}>
         <label className='label2' htmlFor="name">Course Name</label>
         <input
           className='courseName'
@@ -69,6 +84,14 @@ const BusinessPost = (props) => {
             value={price}
             onChange={(e) => setPrice(e.target.value)}
             placeholder='Price'>
+        </input>
+
+        <label className='label2' htmlFor="length">Price</label>
+        <input
+            className='length'
+            value={length}
+            onChange={(e) => setLength(e.target.value)}
+            placeholder='Length of Course'>
         </input>
 
         <label>Website Link</label>
@@ -112,7 +135,7 @@ const BusinessPost = (props) => {
             showCheckbox
         />
 
-          <button /*onClick={event => window.location.href='/'}*/ type='submit'>Post Bootcamp</button>
+          <button onClick={event => window.location.href='/'} type='submit'>Post Bootcamp</button>
 
     </form>
 </div>
