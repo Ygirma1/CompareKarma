63f3fa88c3c11a537595e716e9022c9e06523d10
rchavezyanez3 <rchavezyanez3@gatech.edu>
2023-04-07 08:35:09 -0400

    cats

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,24 +1,64 @@
 import axios from 'axios';
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const Profile = () => {
+useEffect( () => {
+  fetch('http://localhost:8080/imagepath?business_id='+localStorage.getItem("business_id"), {
+  
+  method:'GET',
+  headers: {
+  //"Content-Type": 'application/json, charset=UTF-8',
+  "Accept": "application/json, text/html",
+  
+  }, credentials:"include"
+  
+    }).then(data=> data.json())
+    .then((data)=> {
+  console.log(data)
+  setImages("http://localhost:8080/getimg/"+ data.image)
+  console.log(images)
+  
+    });
+  })
+  const navigate = useNavigate();
     const [image, setImage] = useState({ preview: '', data: '' })
+    const [images, setImages] = useState("")
   const [status, setStatus] = useState('')
+  const [uploadStatus, setUploadStatus] = useState('');
 
-
-  const handleSubmit =  (e) => {
+  const handleSubmit =  (e) => {  
     e.preventDefault()
     let formData = new FormData()
-    console.log(image.data);
-  
     formData.append('file', image.data)
-    const response = axios.put('http://localhost:8080/image',formData)
-    if (response) setStatus(response.statusText)
+    
+  
+ 
+    const response = fetch('http://localhost:8080/image?business_id='+localStorage.getItem("business_id"),{
+
+      method:"POST",
+      body: formData,
+      headers:{
+          "Accept": "multipart/form-data",
+
+      },
+      credentials:"include"
+
+    }).then(res=>res.json())
+    .then(res=>{
+      
+     setUploadStatus(res.msg)
+    // navigate("/");
+    }).catch(error=>{
+
+console.error(error)
+
+    })
   }
 
-  const handleFileChange = (e) => {
+
+  const handleFileChange = (e) => {  // this just changes the file image when you upload it
     const img = {
       preview: URL.createObjectURL(e.target.files[0]),
       data: e.target.files[0],
@@ -36,6 +76,11 @@ const Profile = () => {
         <button type='submit'>Submit</button>
       </form>
       {status && <h4>{status}</h4>}
+      {images && <img src={images} alt="img"/>}
+  
+      
+        <button type='submit'>Submit</button>
+    
     </div>
   
   );
