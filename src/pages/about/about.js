d2f3eefaf1bb401bde4e825e5664fe876bf5484a
Annette Pan <apan38@gatech.edu>
2023-04-20 20:35:41 -0400

    Styling about page

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,49 +1,32 @@
 import React from 'react';
 import './about.css';
+import { ReactComponent as PeopleTalkingSVG } from './people_talking.svg';
+import { ReactComponent as WomanStandingSVG } from './woman_standing.svg';
 
 
 const About = () => {
   return (
     <body>
       <div className='header'>
-        <h1>About</h1>
+        <PeopleTalkingSVG/>
+        <h2>Who We Are: CompareKarma's Mission</h2>
+        <p>In a world where opportunities are overwhelming, CompareKarma aims to be the most effective online education search tool on the market. 
+        </p>
       </div>
 
-      <div className='row'>
-        <div className='leftcolumn'>
-          <div className='card2'>
-            <h2>How we started</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua. Amet dictum sit amet justo donec enim diam vulputate. Erat nam at lectus urna duis convallis convallis
-              tellus id. Mattis pellentesque id nibh tortor id aliquet lectus. Tempus quam pellentesque nec nam. Id venenatis a
-              condimentum vitae sapien. Non blandit massa enim nec dui nunc mattis enim. Lorem dolor sed viverra ipsum nunc
-              aliquet bibendum enim. Platea dictumst vestibulum rhoncus est pellentesque. Ipsum dolor sit amet consectetur
-              adipiscing elit. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Dictum varius duis at
-              consectetur lorem donec massa sapien. Eget dolor morbi non arcu risus quis varius. Tortor condimentum 
-              lacinia quis vel. Nulla aliquet enim tortor at auctor urna. Augue interdum velit euismod in pellentesque massa
-              placerat duis. Ut eu sem integer vitae justo eget. Vel orci porta non pulvinar neque laoreet suspendisse
-              interdum.
-            </p>
-          </div>
-          <div className='card2'>
-            <h2>Why choose CompareKarma?</h2>
-            <p>Lorem ipsum dolor sit amet. Ut minus delectus sed galisum consequatur qui dolorem voluptatibus qui totam quae.
-              Et odio accusantium ab recusandae magnam ut minima alias. Aut dolor molestiae 33 quas consequuntur et alias
-              voluptatibus nam itaque aliquam! Eos eligendi voluptatem ut excepturi obcaecati qui accusantium quia et soluta
-              reprehenderit non iure dolorem!
-            </p>
-          </div>
-       </div>
-       <div className='rightcolumn'>
-        <div className='card2'>
-          <h2>Features</h2>
-          <h5>Feature 1</h5>
-          <h5>Feature 2</h5>
-          <h5>Feature 3</h5>
-          <h5>Feature 4</h5>
-        </div>
-       </div>
+      <div className='row1'>
+      <h2>How We Started</h2>
+        <p>ComparKarma began with the act of contextual research. What began as a series of interviews with people from all walks of life transformed
+          into an idea that could save many people precious time and money that could be better used elsewhere. After understanding the motivations
+          behind why people choose to learn industry-standards skills, we were able to understand that most of the job demand is concentrated in the
+          tech industry. With this information, we knew what to pursue next: creating a tool that allows people to better find learning opportunities
+          that suit their time and budget demands.</p>
       </div>
+
+      <div className='row2'>
+        <h2>CompareKarma Offers The Following Features:</h2>
+      </div>
+
     </body>
   );
 };
