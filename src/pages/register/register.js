d2d41b3312c1762a5e112ac1a422393b405c0fea
romanchavezyanez <chavez.roman50@gmail.com>
2023-04-21 10:27:48 -0400

    fixed bug where user stayed logged in after closing tab

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -42,7 +42,7 @@ const Register = (props) => {
         '&course_type=' + newCourseTypes + 
         '&business_password=' + pass)
         .then(response => {
-            localStorage.setItem('business_id', JSON.stringify(response.data.business_id));
+            sessionStorage.setItem('business_id', JSON.stringify(response.data.business_id));
             props.onLogin();
             window.location.href='/';
         })
